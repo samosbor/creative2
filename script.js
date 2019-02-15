e6da288c8fe9b582b99679378bb1d02e1f9b0d4f
Sam Osborne <sosborne@simplenexus.com>
2019-02-14 23:35:06 -0700

    got the js running

---
+++ script.js
@@ -0,0 +1,17 @@
+url = "https://cat-fact.herokuapp.com/facts/random?animal=cat&amount=7"
+fetch(url, {
+    mode: 'cors',
+    headers: {
+      'Access-Control-Allow-Origin':'*'
+    }
+  })
+.then(function(response) {
+    return response.json();
+}).then(function(json) {	
+    var count = 0;
+    json.forEach(element => {
+        count++;
+        var node = document.createTextNode(element.text); 
+        document.getElementById("fact"+count).appendChild(node)
+    });
+});
