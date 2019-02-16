997c2eee73a3494400569072123a6722c5addf3f
Sam Osborne <sosborne@simplenexus.com>
2019-02-15 22:16:00 -0700

    b

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 url = "https://cat-fact.herokuapp.com/facts/random?animal=cat&amount=7"
 fetch(url, {
-    mode: 'no-cors'
+    mode: 'cors'
   })
 .then(function(response) {
     return response.json()
